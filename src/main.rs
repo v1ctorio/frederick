fa58a6c29870d65faf39f0607e5623a5796742ed
vic <74506415+v1ctorio@users.noreply.github.com>
2024-08-24 19:19:16 +0200

    badfix

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use constants::generate_configuration;
 use owo_colors::OwoColorize;
 use reqwest::header::{self, ACCEPT};
 use serde::Deserialize;
-use tokio::io::{self, AsyncBufReadExt, AsyncWriteExt};
-use std::fs::File;
 use std::fs;
+use std::fs::File;
+use tokio::io::{self, AsyncBufReadExt, AsyncWriteExt};
 use toml::value::Array;
 use urlencoding::encode;
 
@@ -39,8 +39,6 @@ struct TrackData {
 
 pub const BASE_BRAINZ_URL: &str = "https://musicbrainz.org/ws/2";
 
-
-
 #[tokio::main]
 async fn main() {
     let api_client = build_api_client();
@@ -57,15 +55,26 @@ async fn main() {
         .expect("No file provided. Use the --file flag to provide a flag.");
     let file = std::path::Path::new(&file);
 
-
-    let mut song_name = file.file_stem().expect("File to obtain file name").to_str().unwrap().to_string();
-    let file_extension = file.extension().expect("Failed to retrive file extension").to_str().unwrap();
+    let mut song_name = file
+        .file_stem()
+        .expect("File to obtain file name")
+        .to_str()
+        .unwrap()
+        .to_string();
+    let file_extension = file
+        .extension()
+        .expect("Failed to retrive file extension")
+        .to_str()
+        .unwrap();
 
     if let Some(n) = args.release_name {
         song_name = n;
     }
 
-    println!("The release name im searching for is {:?}", song_name.on_blue());
+    println!(
+        "The release name im searching for is {:?}",
+        song_name.on_blue()
+    );
     println!("The file extension is {:?}", file_extension);
 
     if !file.is_file()
@@ -75,23 +84,14 @@ async fn main() {
         return;
     }
 
-
-    let found_data = get_song_data(
-        &api_client,
-        song_name,
-    );
+    let found_data = get_song_data(&api_client, song_name);
 
     let mut found_data = found_data.await.unwrap();
 
-    println!(
-        "The file I'm going to edit is {:?}",
-        &file.on_yellow()
-    );
-
-
+    println!("The file I'm going to edit is {:?}", &file.on_yellow());
 
     if (&found_data.releases).is_empty() {
-        println!("{}","No data found for the release name provided.".red());
+        println!("{}", "No data found for the release name provided.".red());
 
         let mut reader = io::BufReader::new(io::stdin());
         let mut input = String::new();
@@ -99,21 +99,29 @@ async fn main() {
         reader.read_line(&mut input).await.unwrap();
         let input = input.trim();
 
-        found_data = get_song_data(
-            &api_client,
-            input.to_string(),
-        ).await.unwrap();
+        found_data = get_song_data(&api_client, input.to_string()).await.unwrap();
     }
     let mut file_handle = File::open(&file).unwrap();
 
-
     let chosen_release = &found_data.releases.first().unwrap();
-    println!("The following data has been found: title: {} year: {}, country: {}", &chosen_release.title.on_green(), &chosen_release.date.as_ref().unwrap().on_green(),&chosen_release.country.as_ref().unwrap().on_green());
+    
+    
+    let chosen_title = &chosen_release.title;
+    let chosen_date: i32 = chosen_release.date.as_ref().unwrap()[0..4].parse().unwrap();
+
 
     let mut new_tag = Tag::new().read_from_path(file).unwrap();
-    new_tag.set_title(&chosen_release.title);
-    new_tag.set_year(chosen_release.date.as_ref().unwrap()[0..4].parse().unwrap());
+    new_tag.set_title(chosen_title);
+    new_tag.set_year(chosen_date);
 
-    new_tag.write_to(&mut file_handle).expect("Error writing to the file.");
+    new_tag
+        .write_to(&mut file_handle)
+        .expect("Error writing to the file.");
     println!("The file has been tagged with the new data.");
+    println!(
+        "The following data has been found: title: {} year: {}, country: {}",
+        chosen_title,
+        chosen_date,
+        &chosen_release.country.as_ref().unwrap().on_green()
+    );
 }
